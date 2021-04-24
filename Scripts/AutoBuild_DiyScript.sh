14af0487f56f639e20c75c1de9795740b8c45e53
backup <6046642+mlpzaq9998@users.noreply.github.com>
2021-04-24 23:35:33 +0800

    1

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -6,28 +6,28 @@
 Diy_Core() {
 	Author=ZYL
 	Default_Device=x86_64
+	Short_Firmware_Date=true
+	Default_IP_Address=192.168.3.1
 
 	INCLUDE_AutoUpdate=true
 	INCLUDE_AutoBuild_Tools=true
 	INCLUDE_DRM_I915=true
-	INCLUDE_Translation_Converter=
+	INCLUDE_Theme_Argon=true
+	INCLUDE_Obsolete_PKG_Compatible=false
 }
 
 Firmware-Diy() {
-	Update_Makefile exfat package/kernel/exfat
-	Replace_File CustomFiles/webadmin.po package/lean/luci-app-webadmin/po/zh-cn
-	
+	Update_Makefile exfat $(PKG_Finder d package exfat)
+	AddPackage svn ../feeds/packages/admin netdata https://github.com/openwrt/packages/trunk/admin
+
 	case ${TARGET_PROFILE} in
 	d-team_newifi-d2)
 		Replace_File CustomFiles/mac80211.sh package/kernel/mac80211/files/lib/wifi
-		Replace_File CustomFiles/system_newifi-d2 package/base-files/files/etc/config system
-		Replace_File CustomFiles/102-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.4
+		Replace_File CustomFiles/system_d-team_newifi-d2 package/base-files/files/etc/config system
+		# Replace_File CustomFiles/Patches/102-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.4
 	;;
-	*)
-		Replace_File CustomFiles/system_common package/base-files/files/etc/config system
-	;;
-	esac		
-		
+	esac
+	
 	rm -rf package/lean/luci-app-wrtbwmon
 
 	cp -r -f $GITHUB_WORKSPACE/CustomFiles/luci-app-dockerman ./package/lean/luci-app-dockerman
@@ -38,8 +38,4 @@ Firmware-Diy() {
 	chmod -R 755 ./package/lean/luci-app-koolproxyR
 	chmod -R 755 ./package/lean/koolproxy
 	chmod -R 755 ./package/lean/luci-app-dockerman
-	
-	sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-	sed -i 's/bootstrap/argon/g' feeds/luci/modules/luci-base/root/etc/config/luci
 }
-
