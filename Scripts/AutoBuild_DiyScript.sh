1a5645964a5f95d80ea9adce8086d77b6ba150e7
backup <6046642+mlpzaq9998@users.noreply.github.com>
2021-05-08 01:37:40 +0800

    1

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -37,7 +37,6 @@ Firmware-Diy() {
 	cp -r -f $GITHUB_WORKSPACE/CustomFiles/luci-app-cupsd ./package/lean/luci-app-cupsd
 	cp -r -f $GITHUB_WORKSPACE/CustomFiles/cups ./package/lean/cups
 	cp -r -f $GITHUB_WORKSPACE/CustomFiles/cups-bjnp ./package/lean/cups-bjnp
-	cp -r -f $GITHUB_WORKSPACE/CustomFiles/hplip ./package/lean/hplip
 	cp -r -f $GITHUB_WORKSPACE/CustomFiles/luci-app-dockerman ./package/lean/luci-app-dockerman
 	cp -r -f $GITHUB_WORKSPACE/CustomFiles/luci-app-wrtbwmon ./package/lean/luci-app-wrtbwmon
 	cp -r -f $GITHUB_WORKSPACE/CustomFiles/wrtbwmon ./package/lean/wrtbwmon
@@ -49,5 +48,4 @@ Firmware-Diy() {
 	chmod -R 755 ./package/lean/luci-app-cupsd
 	chmod -R 755 ./package/lean/cups
 	chmod -R 755 ./package/lean/cups-bjnp
-	chmod -R 755 ./package/lean/hplip
 }
